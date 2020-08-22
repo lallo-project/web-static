3340bb9cf7e9f5756c2e28be2960ece6c8521e7a
Muhammad Wafa <wafax.4@gmail.com>
2020-08-22 20:29:49 +0700

    navbar change color

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  var scroll_start = 0;
+  var startchange = $('.hero');
+  var offset = startchange.offset();
+  if (startchange.length) {
+    $(document).scroll(function () {
+      scroll_start = $(document).scrollTop();
+      if (scroll_start > offset.top) {
+        $(".navbar").css('background-color', 'var(--tosca)');
+        $(".navbar").css('box-shadow', '3px 3px 5px #aaa');
+        $(".navbar .btn-tosca").css('background-color', 'white');
+        $(".navbar .btn-tosca").css('color', 'var(--tosca)');
+      } else {
+        $(".navbar").css('box-shadow', 'none');
+        $('.navbar').css('background-color', 'transparent');
+        $(".navbar .btn-tosca").css('background-color', 'var(--tosca)');
+        $(".navbar .btn-tosca").css('color', 'white');
+      }
+    });
+  }
+});
