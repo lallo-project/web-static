8559e0ab12ce3fd48bc58ba54e3dbafefc0b6d67
Muhammad Wafa <wafax.4@gmail.com>
2020-08-23 14:05:29 +0700

    update navbar change mechanism

--- js/script.js
+++ js/script.js
@@ -6,15 +6,13 @@ $(document).ready(function () {
     $(document).scroll(function () {
       scroll_start = $(document).scrollTop();
       if (scroll_start > offset.top) {
-        $(".navbar").css('background-color', 'var(--tosca)');
-        $(".navbar").css('box-shadow', '3px 3px 5px #11111145');
-        $(".navbar .btn-tosca").css('background-color', 'white');
-        $(".navbar .btn-tosca").css('color', 'var(--tosca)');
+        $(".navbar.navbar-dyn").addClass('navbar-tosca');
       } else {
-        $(".navbar").css('box-shadow', 'none');
-        $('.navbar').css('background-color', 'transparent');
-        $(".navbar .btn-tosca").css('background-color', 'var(--tosca)');
-        $(".navbar .btn-tosca").css('color', 'white');
+        $(".navbar.navbar-dyn").removeClass('navbar-tosca');
+        // $(".navbar").css('box-shadow', 'none');
+        // $('.navbar').css('background-color', 'transparent');
+        // $(".navbar .btn-tosca").css('background-color', 'var(--tosca)');
+        // $(".navbar .btn-tosca").css('color', 'white');
       }
     });
   }
